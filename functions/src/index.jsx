010aa4e570c9505f328e8f8262a90fa9df16c9aa
ridermansb <ridermansb@gmail.com>
2020-06-20 18:23:40 -0300

    Fix deployment

--- functions/src/index.jsx
+++ functions/src/index.jsx
@@ -15,35 +15,12 @@ const app = express();
 app.use(compression({ threshold: 0 }))
 app.use(cors({origin: true}));
 
-// Sitemap route
-app.get('/sitemap.xml', function(req, res) {
-    res.set('Cache-Control', 'public, max-age=600, s-maxage=1800');
-    res.header('Cache-Control', 'public, max-age=600, s-maxage=1800');
-    res.header('Content-Type', 'application/xml');
-    res.header('Content-Encoding', 'gzip');
-
-    try {
-        const smStream = new SitemapStream({ hostname: 'https://react-firebase-ssr.ridermansb.dev' })
-        const pipeline = smStream.pipe(createGzip())
-        smStream.write({ url: '/',  changefreq: 'daily', priority: 0.7 })
-        smStream.end()
-        // cache the response
-        streamToPromise(pipeline);
-        // stream write the response
-        pipeline.pipe(res).on('error', (e) => {throw e})
-    } catch (e) {
-        console.error(e)
-        res.status(500).end()
-    }
-})
-
 const publicFolder = path.resolve(__dirname, './public');
 const indexHtmlPath = path.join(publicFolder, 'client.html');
 const htmlIndex = fs.readFileSync(indexHtmlPath, 'utf8');
 
 const serverRenderer = (req, res) => {
     res.set('Cache-Control', 'public, max-age=60, s-maxage=180');
-    res.header('Cache-Control', 'public, max-age=60, s-maxage=180');
     
     const html = renderToString(<App />)
     // console.log('Rendering helmet ...', html);
@@ -62,4 +39,26 @@ const runtimeOpts = {
     memory: '512MB'
 }
 exports.ssr = functions.runWith(runtimeOpts).https.onRequest(app);
+
+// Sitemap route
+exports.sitemap = functions.https.onRequest((req, resp) => {
+    res.set('Cache-Control', 'public, max-age=600, s-maxage=1800');
+    res.set('Content-Type', 'application/xml');
+    res.set('Content-Encoding', 'gzip');
+
+    try {
+        const smStream = new SitemapStream({ hostname: 'https://react-firebase-ssr.ridermansb.dev' })
+        const pipeline = smStream.pipe(createGzip())
+        smStream.write({ url: '/',  changefreq: 'daily', priority: 0.7 })
+        smStream.end()
+        // cache the response
+        streamToPromise(pipeline);
+        // stream write the response
+        pipeline.pipe(res).on('error', (e) => {throw e})
+    } catch (e) {
+        console.error(e)
+        res.status(500).end()
+    }
+    
+});
 // exports.ssr = functions.https.onRequest(app);
