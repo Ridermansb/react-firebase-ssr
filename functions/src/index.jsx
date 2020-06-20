c320e5729b79d86cff78c0446a1a890e42234038
ridermansb <ridermansb@gmail.com>
2020-06-20 19:37:48 -0300

    Fix deployment

--- functions/src/index.jsx
+++ functions/src/index.jsx
@@ -42,23 +42,18 @@ exports.ssr = functions.runWith(runtimeOpts).https.onRequest(app);
 
 // Sitemap route
 exports.sitemap = functions.https.onRequest((req, resp) => {
-    res.set('Cache-Control', 'public, max-age=600, s-maxage=1800');
-    res.set('Content-Type', 'application/xml');
-    res.set('Content-Encoding', 'gzip');
+    resp.set('Cache-Control', 'public, max-age=600, s-maxage=1800');
+    resp.set('Content-Type', 'application/xml');
+    resp.set('Content-Encoding', 'gzip');
 
-    try {
-        const smStream = new SitemapStream({ hostname: 'https://react-firebase-ssr.ridermansb.dev' })
-        const pipeline = smStream.pipe(createGzip())
-        smStream.write({ url: '/',  changefreq: 'daily', priority: 0.7 })
-        smStream.end()
-        // cache the response
-        streamToPromise(pipeline);
-        // stream write the response
-        pipeline.pipe(res).on('error', (e) => {throw e})
-    } catch (e) {
-        console.error(e)
-        res.status(500).end()
-    }
+    const smStream = new SitemapStream({ hostname: 'https://react-firebase-ssr.ridermansb.dev' })
+    const pipeline = smStream.pipe(createGzip())
+    smStream.write({ url: '/',  changefreq: 'daily', priority: 0.7 })
+    smStream.end()
+    // cache the response
+    streamToPromise(pipeline);
+    // stream write the response
+    pipeline.pipe(resp).on('error', (e) => {throw e})
     
 });
 // exports.ssr = functions.https.onRequest(app);
